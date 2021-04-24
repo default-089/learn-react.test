da6e65b1cbf5ec79b649bd9d90279df92cec5af1
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-04-24 21:01:27 +0300

    forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Fcomponent from './Fcomponent';
 import Ccomponent from './Ccomponent';
 import Afcomponent from './Afcomponent';
 import Counter from './Counter';
+import NewForm from './NameForm';
+import FlavorForm from './FlavorForm';
+import Reservation from './Reservation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Fcomponent name="Andrey" />
+        <NewForm />
         <Counter />
+        <FlavorForm />
+        <Reservation />
         <Ccomponent number="456" numbers={[2,3,5,6]} />
         <Afcomponent />
         <img src={logo} className="App-logo" alt="logo" />
--- src/Fcomponent.js
+++ src/Fcomponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Fcomponent(props) {
   return (
     <div>
-      <h1>Hello, {props.name}, ({props.age})</h1>
+      <h1>Hello, {props.name}{props.name.length > 0 ? "," : ""} ({props.age})</h1>
     </div>
   )
 }
